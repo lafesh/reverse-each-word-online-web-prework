9921dac03c77326f07a808ed3486eb2a246337fe
Gina Fedder <ginafedder@yahoo.com>
2018-09-17 22:56:49 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,4 @@
-def reverse_each_word(sentence)
-  array = sentence.split(" ")
-  new_array = []
-  array.each do |word|
-    new_array << word.reverse
-  end
-  return new_array.join(" ")
-end
+
 
 def reverse_each_word(sentence)
   array2 = sentence.split(" ")
